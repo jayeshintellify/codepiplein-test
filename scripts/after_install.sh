070cce103b53bd0c573e01ad0132010eea6f9ab7
awspipeline-at-727668044432 <>
2025-01-27 12:02:03 +0530

    new updates

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # Unzip the build artifact to the destination folder
 echo "Unzipping new build..."
-sudo unzip /react-app-build.zip -d /var/www/healthc2u-admin/
-
-# Set correct permissions for the files if needed
-echo "Setting permissions..."
-sudo chmod -R 755 /var/www/healthc2u-admin/
+sudo unzip build.zip -d /var/www/healthc2u-admin/ && mv /var/www/healthc2u-admin/build/* /var/www/healthc2u-admin/ && rm -r /var/www/healthc2u-admin/build
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,5 +2,3 @@
 # Clear previous build artifacts
 echo "Removing old build artifacts..."
 sudo rm -rf /var/www/healthc2u-admin/*
-
-# You can also stop the web server here if you need to update the server configuration
