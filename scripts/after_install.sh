ec911922e594efbb77b07fbcbf103ad21b0a3be1
awspipeline-at-727668044432 <>
2025-01-27 12:33:19 +0530

    afterinstall update

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Unzip the build artifact to the destination folder
 echo "Unzipping new build..."
-sudo unzip build.zip -d /var/www/healthc2u-admin/ && mv /var/www/healthc2u-admin/build/* /var/www/healthc2u-admin/ && rm -r /var/www/healthc2u-admin/build
+sudo unzip /var/www/healthc2u-admin/build.zip -d /var/www/healthc2u-admin/ && mv /var/www/healthc2u-admin/build/* /var/www/healthc2u-admin/ && rm -r /var/www/healthc2u-admin/build && rm -r /var/www/healthc2u-admin/build.zip
